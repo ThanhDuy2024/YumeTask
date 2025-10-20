a758982c0bb285b4b43ac0480b00c8861b406a35
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-10-20 07:26:48 +0700

    [Feature] account login basic

--- routes/account.route.ts
+++ routes/account.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createAccount } from "../controllers/account.controller";
+import { createAccount, login } from "../controllers/account.controller";
 const router = Router();
 
 router.post("/create", createAccount);
 
+router.post("/login", login);
 export default router;
