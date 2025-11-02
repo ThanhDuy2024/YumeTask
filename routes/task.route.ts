c6151bb7ccffa6be8e2e397bcf5380fd2621661f
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-02 09:39:53 +0700

    [Update] task validate

--- routes/task.route.ts
+++ routes/task.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createTask, deleteTask, taskList, updateTask } from "../controllers/task.controller";
+import { taskValidate } from "../validates/task.validate";
 
 const router = Router();
 
-router.post("/create", createTask);
+router.post("/create", taskValidate, createTask);
 
 router.get("/list", taskList);
 
-router.put("/update/:id", updateTask);
+router.put("/update/:id", taskValidate, updateTask);
 
 router.delete("/delete/:id", deleteTask);
 export default router;
