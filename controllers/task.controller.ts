dffb6de907148109b12492e78bf1ebb53f6bbbf1
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-02 08:56:16 +0700

    [Update] create task and get task

--- controllers/task.controller.ts
+++ controllers/task.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { users } from "../interfaces/account.interface";
+import { Task } from "../models/Task.model";
+import moment from "moment";
 
-export const createTask = async (req: Request, res: Response ) => {
+export const createTask = async (req: users, res: Response ) => {
   try {
+    const task: any = req.body;
+
+    task.userId = req.users.id;
+    
+    await Task.create(task);
+
     res.status(200).json({
       code: "success",
       message: "Tạo mục nhiệm vụ thành công"
@@ -15,12 +24,35 @@ export const createTask = async (req: Request, res: Response ) => {
   }
 };
 
-export const taskList = async (req: Request, res: Response) => {
+export const taskList = async (req: users, res: Response) => {
   try {
+
+    const list = await Task.find({
+      userId: req.users.id
+    });
+
+    const finalData:any = [];
+    for (const item of list) {
+      const rawData:any = {
+        id: item.id,
+        userId: item.userId,
+        taskContent: item.taskContent,
+        status: item.status,
+        createdAt: "",
+        updatedAt: ""
+      };
+
+      rawData.createdAt = moment(item.createdAt).format("HH:mm DD/MM/YYYY");
+      rawData.updatedAt = moment(item.updatedAt).format("HH:mm DD/MM/YYYY");
+
+      finalData.push(rawData);
+    };
+    
     res.status(200).json({
       code: "success",
-      data: []
-    })
+      data: finalData,
+    });
+
   } catch (error) {
     console.log(error);
     res.status(400).json({
