dbaece4c5af1b53523aa9d8f0129320f8d1ed822
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-07 13:05:34 +0700

    [Update] cors

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors"
 import "dotenv/config"
 import routeClient from "./routes/index.route";
 import { connectDatabase } from "./config/database";
 const port = process.env.PORT;
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 connectDatabase();
